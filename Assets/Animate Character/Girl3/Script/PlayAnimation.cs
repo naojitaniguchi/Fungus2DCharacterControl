b79a9df3846b6d95b3bb8d52449c27b7058a96f6
naojitaniguchi <naoji.taniguchi@gmail.com>
2018-09-30 20:52:28 +0900

    first commit .

---
+++ Assets/Animate Character/Girl3/Script/PlayAnimation.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimation : MonoBehaviour {
+
+  
+    public Animator Ani;
+    public Sprite[] Spr;
+    public SpriteRenderer sprR;
+   
+    public bool onlySpr = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PlayAni(string str)
+    {
+       
+        Ani.enabled = true;
+        Ani.ForceStateNormalizedTime(0.0f);
+        Ani.Play(str);
+
+    }
+
+
+
+
+    public void ChangeSpr(int num)
+    {
+        if (onlySpr)
+        {
+            
+        }
+        else {
+           
+            Ani.ForceStateNormalizedTime(0.0f);
+            Ani.enabled = false;           
+        }
+
+        sprR.sprite = Spr[num];
+    }
+
+    public void ChangeSpr0()
+    {
+        sprR.sprite = Spr[0];
+    }
+
+    public void ChangeSpr1()
+    {
+        sprR.sprite = Spr[1];
+    }
+    public void ChangeSpr2()
+    {
+        sprR.sprite = Spr[2];
+    }
+    public void ChangeSpr3()
+    {
+        sprR.sprite = Spr[3];
+    }
+    public void ChangeSpr4()
+    {
+        sprR.sprite = Spr[4];
+    }
+    public void ChangeSpr5()
+    {
+        sprR.sprite = Spr[5];
+    }
+    public void ChangeSpr6()
+    {
+        sprR.sprite = Spr[6];
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Fungus2DCharacterControl
+
+[![For one cube](http://img.youtube.com/vi/lHq9c63Ua-I/0.jpg)](http://www.youtube.com/watch?v=lHq9c63Ua-I)
+
+Controlling character facial expression By Fungus.
+
+Sample Unity Fungus project controlling 2D Character  facial expression .
+
+I'm using 2D character from this asset .
+https://assetstore.unity.com/packages/2d/characters/2d-animate-girl-character3-101361
+
+I added function to change sprite from Fungus , and I added Call Method command from Fungus flowchart block .
