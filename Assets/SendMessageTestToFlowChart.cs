619d371db468549b0814eb517475515ba9ecbae5
naojitaniguchi <naoji.taniguchi@gmail.com>
2018-10-03 10:21:38 +0900

    added sample for starting Fungus flow from Unity Script .

---
+++ Assets/SendMessageTestToFlowChart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class SendMessageTestToFlowChart : MonoBehaviour {
+    // public Flowchart f;
+    public GameObject flowchartObj;
+
+	// Use this for initialization
+	void Start () {
+        //f.SendMessage("Start");
+        // flowchartObj.SetActive(true);
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if ( Input.GetKeyDown(KeyCode.A)){
+            flowchartObj.SetActive(true);
+        }
+	}
+}
